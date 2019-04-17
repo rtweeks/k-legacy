c620d6f80c18bd1b307559497c6dcad747e057d9
Richard T. Weeks <rtweeks21@gmail.com>
2019-04-16 17:57:13 -0700

    Add the parsing coordinator class
    This class will be injected through Guice to coordinate the sharing of
    the parser object instantiated by `KRun`.

---
+++ java-backend/src/main/java/org/kframework/backend/java/builtins/AdditionalParsingCoordinator.java
@@ -0,0 +1,27 @@
+package org.kframework.backend.java.builtins;
+
+import org.kframework.kore.K;
+import org.kframework.kore.Sort;
+
+public class AdditionalParsingCoordinator {
+  public interface Provider {
+    public K apply(String filePath, String module, Sort startSymbol);
+  }
+
+  private Provider provider;
+
+  public AdditionalParsingCoordinator() {
+    provider = null;
+  }
+
+  public void setProvider(Provider provider) {
+    this.provider = provider;
+  }
+
+  public K parse(String filePath, String module, Sort startSymbol) {
+    if (provider == null) {
+      throw new RuntimeException("Not implemented!");
+    }
+    return provider.apply(filePath, module, startSymbol);
+  }
+}
